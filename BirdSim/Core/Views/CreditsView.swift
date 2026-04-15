12027cc858a2a6f5113a1c193d562c138949aacb
jaidenhenley <jaidenhenley@gmail.com>
2026-04-15 09:25:13 -0400

    [Feature] Added CreditsView to display credits information
    - Created new file `CreditsView.swift` in `BirdSim/Core/Views/`
    - Added `CreditsView` struct implementing `View` protocol
    - Included a `Text` view with placeholder text "Hello, World!"
    - Added a preview for `CreditsView`

---
+++ BirdSim/Core/Views/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  BirdSim
+//
+//  Created by Jaiden Henley on 4/15/26.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
